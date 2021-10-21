f4011ba2705b950723dbcb6630f7b64098dc102c
kareem261 <64379540+kareem261@users.noreply.github.com>
2021-10-21 22:47:37 +0200

    Initial commit 5

--- app/Http/Controllers/TestApiController.php
+++ app/Http/Controllers/TestApiController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Kreait\Firebase\Database;
 
 class TestApiController extends Controller
 {
-    public function FirstApi(){
-        return ["name"=>'GP',"email"=>'GP@gmial.com'];
+
+    
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
+    public function add_contact(Request $request){
+
+        $ref_tablename='contacts';
+        $postData = [
+            'fname' =>  $request->input('fname'),
+            'lname' =>  $request->input('lname'),
+        ];
+        $postRef =$this->database->getReference($ref_tablename)->push($postData);
+
+        if ($postRef){
+          return "the process work";
+        }
+        else{
+            return "not work";
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //kda lw 3mlt call ll api def hyrg3ly el response el feh el controller el fe el method bta3t firstApi
-Route::get('first-api',[TestApiController::class,'FirstApi']);
+Route::post('create-contact',[TestApiController::class,'add_contact']);
+
 
 ?>

a24c557d1af1650268c523dfdf0d0d2e60c20494
kareem261 <64379540+kareem261@users.noreply.github.com>
2021-10-22 11:06:45 +0200

    Initial commit 9

--- app/Http/Controllers/TestApiController.php
+++ app/Http/Controllers/TestApiController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Kreait\Firebase\Database;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Message;
 
 class TestApiController extends Controller
 {
@@ -15,6 +18,14 @@ class TestApiController extends Controller
     }
 
     public function add_contact(Request $request){
+
+        $factory = (new Factory())
+    ->withServiceAccount('D:\xampp1\htdocs\myapp\laraveldbtutorial-firebase-adminsdk-amcy9-7c6cb4fca6.json')
+    ->withDatabaseUri('https://laraveldbtutorial-default-rtdb.firebaseio.com/');
+
+    $messaging = $factory->createMessaging();
+    $deviceToken = "12345";
+    
 
         $ref_tablename='contacts';
         $postData = [

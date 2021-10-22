d6923331af9c750ad029927e0dc0ec6f6379db1e
kareem261 <64379540+kareem261@users.noreply.github.com>
2021-10-22 13:21:08 +0200

    Initial commit 9

--- app/Http/Controllers/TestApiController.php
+++ app/Http/Controllers/TestApiController.php
@@ -4,33 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Kreait\Firebase\Database;
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Service;
-use Symfony\Component\Cache\Simple\FilesystemCache;
-
-
 
 class TestApiController extends Controller
 {
-   /* public function __construct(Database $database)
+
+    
+    public function __construct(Database $database)
     {
         $this->database = $database;
-    }*/
+    }
 
-    public function index(Request $request,Database $database){
-        
-         
-        $factory = (new Factory())
-        ->withServiceAccount('D:\xampp1\htdocs\myapp\laraveldbtutorial-firebase-adminsdk-amcy9-7c6cb4fca6.json')
-        ->withDatabaseUri('https://laraveldbtutorial-default-rtdb.firebaseio.com/');    
-           
+    public function add_contact(Request $request){
 
         $ref_tablename='contacts';
         $postData = [
             'fname' =>  $request->input('fname'),
             'lname' =>  $request->input('lname'),
         ];
-        $postRef =$database->getReference($ref_tablename)->push($postData);
+        $postRef =$this->database->getReference($ref_tablename)->push($postData);
 
         if ($postRef){
           return "the process work";
